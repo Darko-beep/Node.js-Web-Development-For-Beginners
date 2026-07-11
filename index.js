e60f594a7714f7017f14d11868f93f3a0b0fab9b
unknown <darkofrederick98@outlook>
2026-07-11 13:12:25 +0000

    Added new feature component

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+require('dotenv').config();
+require('./libs/dbConnect');
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+   res.render('index', { message: 'Hello From Node.js' });
+});
+app.get('/contact', (req, res) => {
+  res.render('index', { message: 'The Contact Page' });
+});
+app.get('/about', (req, res) => {
+  res.render('index', { message: 'The About Page' });
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
